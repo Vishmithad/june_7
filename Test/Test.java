49f8d34fe8e76d45f598de52b94519aad800bdc2
Vishmithad <vishmitha.xworkz@gmail.com>
2023-06-30 13:53:01 +0530

    trying folder

---
+++ Test/Test.java
@@ -0,0 +1,59 @@
+a)29
+c)20
+c)96
+c)63
+a)9
+b)K
+a)p
+b)FR
+C)bccab
+a)GOV
+2
+38
+Window
+SUN
+DF
+                                              java test
+1)java is a object oriented language program can write in once but run in anywhere.javac is a compiler it is platform independent.
+
+2)jdk-java develepment kit it is used to writing the program.
+  jre-java runtime env it is used to execuiting the program.
+  jvm-java vertual machine-it used to execuiting the actual program.
+  
+4)class is a keyword and program, object is real world things.
+
+5)array is a container it is used to retrieving the ref value.
+
+7)6
+
+9)java is a object oriented programin language.
+
+10)variable is a container its stores the data value.
+static variable and nonstatic variable.
+
+11)variable define in a class object is same copy. 
+
+12)premitive data types-short,byte,int,long,
+float,double(decimal values)
+char,boolean(true or false)
+and non premitive data types-real world things.
+
+13)java is a object oriented language.
+ java class is run in jdk,
+ 
+14)implicit and explicit
+
+16)statement. system-pre define class name,out-static variable, println-static method.
+
+17)it is used to the same memory location address of the reference variable in constant pool.
+
+18)dot operator is a local variable it is access the messages.
+
+19)bytecode
+
+20)if,for,int,float,boolean,default,byte,double,char,else.
+
+
+
+
+
