76720c390d32e4003151a58203a75d1ac052d6b1
Vishmithad <vishmitha.xworkz@gmail.com>
2023-07-12 12:12:41 +0530

    project

---
+++ project1/src/com/xworkz/project1/Bus.java
@@ -0,0 +1,10 @@
+package com.xworkz.project1;
+
+public class Bus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("running main class");
+	}
+
+}
