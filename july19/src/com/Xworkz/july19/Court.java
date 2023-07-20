b95871fe936eff30595807097772fd55920f8908
Vishmithad <vishmitha.xworkz@gmail.com>
2023-07-20 10:11:41 +0530

    overriding

---
+++ july19/src/com/Xworkz/july19/Court.java
@@ -0,0 +1,9 @@
+package com.Xworkz.july19;
+
+public class Court {
+		void justice()
+		{
+			System.out.println("invoking justice in Court");
+		}
+	}
+
---
+++ july19/src/com/Xworkz/july19/SupremeCourt.java
@@ -0,0 +1,16 @@
+package com.Xworkz.july19;
+
+public class SupremeCourt extends Court {
+
+		void importantCase()
+		{
+			System.out.println("invoking importanceCase in SupremeCourt");
+		}
+
+		@Override                  // Annotation  
+	   void justice()
+		{
+			System.out.println("ovriding justice method in supremecourt");
+		}
+}
+
